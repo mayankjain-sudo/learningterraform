fc9c7c31830462ca802c8e556e0072ee85a395c3
mayankkeybuzz <mayank.keybuzz@gmail.com>
2022-11-13 14:12:14 +0800

    error updated

--- versionconstraints/main.tf
+++ versionconstraints/main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "hashicorp/local"
       version = "2.2.1"            // version "!=2.2.1" to not use of this version so it wi;; download just previous verison of it
     }                              // version = "> 1.2.0, <2.2.1, != 2.0.0" for multiple conditions                  
-  }                                // version = "~> 1.2" download specific version or just incremental of it.
+  }                                // version = "~> 1.2" download specific version or incremental of it.
 }
 
 resource "local_file" "pets" {
